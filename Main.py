df921feff12d194e1e057e11a2fff42d2ed51d8a
raunak <raunakbiswas05@gmail.com>
2024-12-06 14:50:19 +0530

    Made a minimal pdf file with of invoice

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import pandas
 import glob #is used to make a list of file paths
 from fpdf import FPDF
 import openpyxl #it has to be imported to read xlsx files
+import pathlib
 
 
 filepaths = glob.glob(r"excelFiles\*.xlsx")
@@ -9,4 +10,12 @@ print(filepaths)
 
 for filepath in filepaths:
     data = pandas.read_excel(filepath,sheet_name="Sheet 1")
+    pdf = FPDF(orientation="P",unit="mm",format="A4")
+    filename= pathlib.Path(filepath).stem #shows exact filename
+    invoice_number = filename.split("-")[0]
+    pdf.add_page()
+    pdf.set_font(family="times",style="B",size=15)
+    pdf.cell(w=50,h=8,txt=f"Invoice number: {invoice_number}",ln=1)
+
+    pdf.output(fr"invoicePDFS\{filename}.pdf")
     print(data)
